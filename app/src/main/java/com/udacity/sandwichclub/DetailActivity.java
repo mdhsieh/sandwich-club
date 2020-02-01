9b0a796458bd96bfb4fd8f9c64096cf0c7e0e54e
Michael Hsieh <mdhsieh8@gmail.com>
2020-01-31 16:31:52 -0800

    parsed JSON and populated UI. Textviews arranged with equal spacing and hid empty text

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,9 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +17,28 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    private TextView alsoKnownAsLabel;
+    private TextView placeOfOriginLabel;
+
+    private TextView alsoKnownAsTextView;
+    private TextView placeOfOriginTextView;
+    private TextView ingredientsTextView;
+    private TextView descriptionTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
+        alsoKnownAsLabel = (TextView) findViewById(R.id.textView2);
+        placeOfOriginLabel = (TextView) findViewById(R.id.textView);
+
+        alsoKnownAsTextView = (TextView) findViewById(R.id.also_known_tv);
+        placeOfOriginTextView = (TextView) findViewById(R.id.origin_tv);
+        ingredientsTextView = (TextView) findViewById(R.id.ingredients_tv);
+        descriptionTextView = (TextView) findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +61,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +74,62 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    /* clear array texts
+       because we don't want to keep any
+       previously clicked sandwich data */
+    private void clearViews()
+    {
+        alsoKnownAsTextView.setText("");
+        ingredientsTextView.setText("");
+    }
 
+    /* set visibility of text
+       depending on whether the corresponding
+       TextView is empty or not
+     */
+    private void setVisibility(Sandwich sw)
+    {
+        if (sw.getAlsoKnownAs().isEmpty())
+        {
+            alsoKnownAsLabel.setVisibility(View.GONE);
+            alsoKnownAsTextView.setVisibility(View.GONE);
+        }
+        else
+        {
+            alsoKnownAsLabel.setVisibility(View.VISIBLE);
+            alsoKnownAsTextView.setVisibility(View.VISIBLE);
+        }
+
+        if (sw.getPlaceOfOrigin().equals(""))
+        {
+            placeOfOriginLabel.setVisibility(View.GONE);
+            placeOfOriginTextView.setVisibility(View.GONE);
+        }
+        else
+        {
+            placeOfOriginLabel.setVisibility(View.VISIBLE);
+            placeOfOriginTextView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void populateUI(Sandwich sandwich) {
+
+        clearViews();
+
+        setVisibility(sandwich);
+
+        for (String alsoKnownAs : sandwich.getAlsoKnownAs())
+        {
+            alsoKnownAsTextView.append(alsoKnownAs + "\n");
+        }
+
+        placeOfOriginTextView.setText(sandwich.getPlaceOfOrigin());
+
+        for (String ingredient: sandwich.getIngredients())
+        {
+            ingredientsTextView.append(ingredient + "\n");
+        }
+
+        descriptionTextView.setText(sandwich.getDescription());
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,56 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class JsonUtils {
 
+    private static final String TAG = "JsonUtils";
+
     public static Sandwich parseSandwichJson(String json) {
+        try {
+            JSONObject jsonSandwich = new JSONObject(json);
+            JSONObject nameJsonSandwich = jsonSandwich.getJSONObject("name");
+            String mainName = nameJsonSandwich.getString("mainName");
+
+            ArrayList<String> alsoKnownAsList = new ArrayList<String>();
+            JSONArray alsoKnownAsJsonSandwichArray = nameJsonSandwich.getJSONArray("alsoKnownAs");
+
+            String alsoKnownAsString = null;
+            for (int i = 0; i < alsoKnownAsJsonSandwichArray.length(); i++)
+            {
+                alsoKnownAsString = alsoKnownAsJsonSandwichArray.getString(i);
+                alsoKnownAsList.add(alsoKnownAsString);
+            }
+
+            String placeOfOrigin = jsonSandwich.getString("placeOfOrigin");
+
+            String description = jsonSandwich.getString("description");
+
+            String image = jsonSandwich.getString("image");
+
+            ArrayList<String> ingredients = new ArrayList<String>();
+            JSONArray ingredientsJsonSandwichArray = jsonSandwich.getJSONArray("ingredients");
+            String ingredientString;
+            for (int i = 0; i < ingredientsJsonSandwichArray.length(); i++)
+            {
+                ingredientString = ingredientsJsonSandwichArray.getString(i);
+                ingredients.add(ingredientString);
+            }
+
+            Sandwich sandwich = new Sandwich(mainName, alsoKnownAsList, placeOfOrigin, description, image, ingredients);
+            return sandwich;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "exception occurred", e);
+        }
         return null;
     }
 }
